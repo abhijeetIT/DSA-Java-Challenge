7b7411da96247134fcc7c420946f6bec7dd7f14d
abhijeetIT <173333375+abhijeetIT@users.noreply.github.com>
2025-12-23 13:35:43 +0530

    Day 9 quetion modified in day 10

--- src/arrays/Three3Sum.java
+++ src/arrays/Three3Sum.java
@@ -11,28 +11,57 @@ public class Three3Sum {
 
     static List<List<Integer>> three3Sum(int[] nums) {
 
-//It's a brute force solution it wants more modifications
+        //here is best approach
+        if(nums.length < 3) return new ArrayList<>(); //if array not sufficient
+
         List<List<Integer>> returnValue = new ArrayList<>();
-        int n = nums.length;
 
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {   // ✅ FIX
-                for (int k = j + 1; k < n; k++) {
+        Arrays.sort(nums);
+        for(int i = 0 ; i < nums.length-2; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue; // skip duplicate 'i'
 
-                    if (nums[i] + nums[j] + nums[k] == 0) {
+            int left = i+1, right = nums.length-1;
 
-                        List<Integer> triplet =
-                                Arrays.asList(nums[i], nums[j], nums[k]);
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
 
-                        triplet.sort(null); // duplicate avoid
+                if(sum == 0){
+                    returnValue.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
-                        if (!returnValue.contains(triplet)) { // ✅ FIX
-                            returnValue.add(triplet);         // ✅ FIX
-                        }
-                    }
+                    // skip duplicates for left and right
+                    while(left < right && nums[left] == nums[left+1]) left++;
+                    while(left < right && nums[right] == nums[right-1]) right--;
+
+                    left++;
+                    right--;
+                } else if (sum < 0){
+                    left++;
+                } else {
+                    right--;   // ✅ yahan fix
                 }
             }
         }
+
+
+//It's a brute force solution it wants more modifications
+//        for (int i = 0; i < n - 2; i++) {
+//            for (int j = i + 1; j < n - 1; j++) {   // ✅ FIX
+//                for (int k = j + 1; k < n; k++) {
+//
+//                    if (nums[i] + nums[j] + nums[k] == 0) {
+//
+//                        List<Integer> triplet =
+//                                Arrays.asList(nums[i], nums[j], nums[k]);
+//
+//                        triplet.sort(null); // duplicate avoid
+//
+//                        if (!returnValue.contains(triplet)) { // ✅ FIX
+//                            returnValue.add(triplet);         // ✅ FIX
+//                        }
+//                    }
+//                }
+//            }
+//        }
         return returnValue;
     }
 
